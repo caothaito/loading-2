86ea6839416f7923eec001feb8d9d60164e40637
LEETRAN <trantrongluu.bk56@gmail.com>
2021-03-25 21:04:31 +0700

    add logic to show list new acc

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $('#server').click(function() {
 
 $('#logoRight').click(function() {
     $('#badBook').toggle('slow');
+    showListName(lstNewAcc);
 });
 
 $('#cumsv').click(function() {
@@ -23,6 +24,38 @@ function hideNotify() {
     $("#footerLoading").toggle('hide');
     $('#cumsv').show();
     $('#server').show();
+}
+var lstNewAcc = [
+    {userName: "User 1", charClass: "Elf"},
+    {userName: "User 2", charClass: "Elf"},
+    {userName: "User 3", charClass: "Elf"},
+    {userName: "User 4", charClass: "Elf"},
+    {userName: "User 5", charClass: "Elf"},
+    {userName: "User 6", charClass: "Elf"},
+    {userName: "User 7", charClass: "Elf"},
+    {userName: "User 8", charClass: "Elf"},
+    {userName: "User 9", charClass: "Elf"},
+    {userName: "User 10", charClass: "Elf"},
+]
+
+var PAGE_COUNT = 2;
+var MAX_ITEMS_PER_PAGE = 5;
+
+var ID_ACC_ELEMENT = "new-account-id-";
+var ID_CHAR_CLASS_ELEMENT = "new-account-class-";
+
+function showListName(lstNewAcc){
+    for(let idx = 0; idx < lstNewAcc.length && idx < PAGE_COUNT; idx++) {
+        let arrItemsOfPage = lstNewAcc.slice(idx, (idx+1) * MAX_ITEMS_PER_PAGE);
+        showItemPage(arrItemsOfPage, idx);
+    }
+}
+
+function showItemPage(items, pageNum) {
+    for(let idx = 0; idx < MAX_ITEMS_PER_PAGE; idx ++) {
+        $("#" + ID_ACC_ELEMENT.toString() + (idx+1 + pageNum * MAX_ITEMS_PER_PAGE).toString()).html(items[idx].userName);
+        $("#" + ID_CHAR_CLASS_ELEMENT.toString() + (idx + 1 + pageNum * MAX_ITEMS_PER_PAGE ).toString()).html(items[idx].charClass);
+    }
 }
 
 $(document).ready(function() {
